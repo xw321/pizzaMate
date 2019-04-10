2d2215e177496a0e13b75d919e17f91ea636fcad
xunwang <vincent.1994.wang@gmail.com>
2019-04-10 15:12:20 -0700

    navbar style change

--- imports/ui/NavBar.jsx
+++ imports/ui/NavBar.jsx
@@ -1,48 +1,73 @@
 import React, { Component } from "react";
-
-import { NavLink } from "react-router-dom";
+import { Menu } from "semantic-ui-react";
+//import { NavLink } from "react-router-dom";
 
 import AccountsUIWrapper from "./AccountsUIWrapper.jsx";
 
 export default class NavBar extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      activeItem: "default"
+    };
+  }
+
+  // <div className="">
+  //   <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+  //     <a className="navbar-brand" href="#">
+  //       <span role="img" aria-label="emoji">
+  //         🍕
+  //       </span>
+  //     </a>
+  //     <button
+  //       className="navbar-toggler"
+  //       type="button"
+  //       data-toggle="collapse"
+  //       data-target="#navbarSupportedContent"
+  //       aria-controls="navbarSupportedContent"
+  //       aria-expanded="false"
+  //       aria-label="Toggle navigation"
+  //     >
+  //       <span className="navbar-toggler-icon" />
+  //     </button>
+
+  //     <div className="collapse navbar-collapse" id="navbarSupportedContent">
+  //       <ul className="navbar-nav mr-auto">
+  //         <li className="nav-item active">
+  //           <NavLink className="nav-link" activeClassName="active" to="/">
+  //             <span className="myfont">
+  //               <h3>pizzaMate</h3>
+  //             </span>{" "}
+  //             <span className="nav-link sr-only">(current)</span>
+  //           </NavLink>
+  //         </li>
+  //       </ul>
+  //       <AccountsUIWrapper />
+  //     </div>
+  //   </nav>
+  //   <br />
+  //   <br />
+  // </div>
   render() {
     return (
-      <div className="">
-        <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
-          <a className="navbar-brand" href="#">
-            <span role="img" aria-label="emoji">
-              🍕
-            </span>
-          </a>
-          <button
-            className="navbar-toggler"
-            type="button"
-            data-toggle="collapse"
-            data-target="#navbarSupportedContent"
-            aria-controls="navbarSupportedContent"
-            aria-expanded="false"
-            aria-label="Toggle navigation"
-          >
-            <span className="navbar-toggler-icon" />
-          </button>
-
-          <div className="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul className="navbar-nav mr-auto">
-              <li className="nav-item active">
-                <NavLink className="nav-link" activeClassName="active" to="/">
-                  <span className="myfont">
-                    <h3>pizzaMate</h3>
-                  </span>{" "}
-                  <span className="nav-link sr-only">(current)</span>
-                </NavLink>
-              </li>
-            </ul>
+      <Menu inverted>
+        <Menu.Item
+          name="  pizzaMate"
+          to="/"
+          active={this.state.activeItem === "   pizzaMate"}
+          onClick={() => this.setState({ activeItem: "   pizzaMate" })}
+        >
+          <span role="img" aria-label="emoji">
+            🍕
+          </span>
+          <span id="pizza">pizzaMate</span>
+        </Menu.Item>
+        <Menu.Menu position="right">
+          <Menu.Item>
             <AccountsUIWrapper />
-          </div>
-        </nav>
-        <br />
-        <br />
-      </div>
+          </Menu.Item>
+        </Menu.Menu>
+      </Menu>
     );
   }
 }
