75969f89466c2ed361f93a0b7fa7d1b45f69f701
jimfuego <32339090+jimfuego@users.noreply.github.com>
2019-04-13 12:35:53 -0700

    Update main.js

--- tests/main.js
+++ tests/main.js
@@ -1,5 +1,6 @@
 import assert from "assert";
 
+//It looks like this isn't being used. Clean it up in case someone wants to browse through your code!
 describe("meteor-simple-chat", function () {
   it("package.json has correct name", async function () {
     const { name } = await import("../package.json");
